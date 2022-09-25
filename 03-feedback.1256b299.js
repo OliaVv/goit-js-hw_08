808fbbe715140aa1c7aa163bbc6322ad4341a5c0
OliaVv <97734384+OliaVv@users.noreply.github.com>
2022-09-25 20:02:20 +0000

    Deploying to gh-pages from @ OliaVv/goit-js-hw_08@27486ac79d0b1f73dd7ab04cdd65ff3ed64326c4 🚀

---
+++ 03-feedback.1256b299.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",(e=>{console.log(e.target.name)}));
+//# sourceMappingURL=03-feedback.1256b299.js.map
---
+++ 03-feedback.73e14860.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",(function(e){console.log(e.target.name)}));
+//# sourceMappingURL=03-feedback.73e14860.js.map
