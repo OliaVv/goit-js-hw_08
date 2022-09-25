0e258cb917c3f24eb3d963cb951e9d55a58b6876
Olha Varenyk <97734384+OliaVv@users.noreply.github.com>
2022-09-25 22:24:54 +0200

    3task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,45 @@
-const formRef = document.querySelector(".feedback-form");
+import throttle from 'lodash.throttle';
+import { save, load, remove } from './storage';
+
+const formRef = document.querySelector('.feedback-form');
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+initPage();
 
 const onFormInput = event => {
-    console.log(event.target.name)
+  const { name, value } = event.target;
+
+  let saveData = load(LOCAL_STORAGE_KEY);
+  saveData = saveData ? saveData : {};
+
+  saveData[name] = value;
+
+  save(LOCAL_STORAGE_KEY, saveData);
+};
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
+
+function initPage() {
+  const saveData = load(LOCAL_STORAGE_KEY);
+
+  if (!saveData) {
+    return;
+  }
+  Object.entries(saveData).forEach(([name, value]) => {
+    formRef.elements[name].value = value;
+  });
 }
 
-formRef.addEventListener("input", onFormInput);
+const handleSubmit = event => {
+  event.preventDefault();
 
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+  remove(LOCAL_STORAGE_KEY);
+};
+
+formRef.addEventListener('submit', handleSubmit);
